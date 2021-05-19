7b226cd7a3701bb2a6ccee675f202008b397aab9
batorus <batoru@yahoo.com>
2021-05-19 12:18:24 +0300

    add exceptions

--- src/main/java/com/batorus/container/ContainerApplication.java
+++ src/main/java/com/batorus/container/ContainerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ContainerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ContainerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ContainerApplication.class, args);
+    }
 
 }
